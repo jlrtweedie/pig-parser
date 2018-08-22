010c8e6e1bb2a0af21be20a3b30871ef96953c80
Lauren Tweedie <jamie.lr.tweedie@gmail.com>
2018-08-22 00:16:48 +0000

    Implemented filter function using np masks

--- pig_parser.py
+++ pig_parser.py
@@ -1,28 +1,54 @@
 import sys
-import csv
 import numpy as np
+import numpy.ma as ma
 import string
 
-VARNAMES = dict()
+VARIABLES = dict()
 
 def _load(varname, filename):
-    # with open(filename, newline='') as csvfile:
-    #     fileread = csv.reader(csvfile, delimiter=',')
-    #     return fileread
-    VARNAMES[varname] = np.genfromtxt(filename, delimiter=',', dtype=np.int32)
+    """Loads .csv into numpy array"""
+    VARIABLES[varname] = np.genfromtxt(filename, delimiter=',', dtype=np.int32)
 
 def _dump(varname):
-    for row in VARNAMES[varname]:
-        print(','.join(map(str, row)))
+    """Dumps array to console in .csv format"""
+    # Catch exception for arrays masked to a single row
+    if len(VARIABLES[varname].shape) == 1:
+        print(','.join(map(str, VARIABLES[varname])))
+    # Otherwise iterates rowwise
+    else:
+        for row in VARIABLES[varname]:
+            print(','.join(map(str, row)))
 
-def _filter(varnme):
-    pass
+def _filter(varname1, varname2, column, value, operator):
+    """Generates a numpy mask, masks input array, returns compressed
+        output to variables dict"""
+    # To do: generate masks programatically given operator input
+    # Operators are reversed in mask because true values are masked, not false
+    if operator == '==':
+        mask = np.repeat(VARIABLES[varname1][:,column]==value,
+                         VARIABLES[varname1].shape[1])
+    elif operator == '>':
+        mask = np.repeat(VARIABLES[varname1][:,column]<=value,
+                         VARIABLES[varname1].shape[1])
+    elif operator == '>=':
+        mask = np.repeat(VARIABLES[varname1][:,column]<value,
+                         VARIABLES[varname1].shape[1])
+    elif operator == '<':
+        mask = np.repeat(VARIABLES[varname1][:,column]>=value,
+                         VARIABLES[varname1].shape[1])
+    elif operator == '<=':
+        mask = np.repeat(VARIABLES[varname1][:,column]>value,
+                         VARIABLES[varname1].shape[1])
 
-def _generate(varname):
+    VARIABLES[varname2] = ma.compress_rows(ma.array(VARIABLES[varname1],
+                                           mask=mask))
+
+def _generate(varname1, varname2):
     pass
 
 def check_var(varname):
-    if VARNAMES.get(varname) is not None:
+    """"Checks if varname exists in variables dict"""
+    if VARIABLES.get(varname) is not None:
         return True
     else:
         return False
@@ -32,9 +58,15 @@ def pig_parser(line):
     if elements[0].lower() == 'dump':
         if check_var(elements[1]):
             _dump(elements[1])
+        else:
+            raise IndexError('Variable {} is undefined'.format(elements[1]))
     elif elements[2].lower() == 'load':
-        pass
-    elif elements[2].lower() == 'foreach':
         pass
     elif elements[2].lower() == 'filter':
         pass
+    elif elements[2].lower() == 'foreach':
+        pass
+
+if __name__ == '__main__':
+    # Test functionality
+    _load('a', 'myfile.csv')
